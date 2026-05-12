dde63e75d8d18b9aa939079f550d9a7205cd8655
Inesvvm <inesvvmoreira@gmail.com>
2026-05-12 11:13:22 +0100

    Ajustes nos campos(nome, Email, e modificação do formulário na Idade,  idade) e
    Adição de uma nova verificação para o campo nome(ver se têm 3 letras), adição de uma verificação no campo e-mail (verifica se termina com gmail.com) , alteração na idade mínima,

--- app.js
+++ app.js
@@ -7,16 +7,19 @@ function enviar(){
     if(Nomes.value.trim()==="")//.value.trim() serve para que os campos não fiquem vazios mesmo co o espaço
         alert("Tem de prencher o campo Nome!");
 
+    else if(Nomes.value.length<3)// verifia se o Nome tem no minimo 3 letras.
+        alert("O campo Nome tem de ter no minímo 3 letras!");
+
     else if(Endereco_Email.value.trim()==="")//verifica se o campo não está vazio
         alert("Tem de preencher o campo do email!");
 
-    else if(!Endereco_Email.value.includes('@'))//verifica se campo o tem @
-        alert("Tem de ser um email");
+    else if(!Endereco_Email.value.includes('@') || !Endereco_Email.value.endsWith('gmail.com'))//verifica se campo o tem @ e termina com gmail.com
+        alert("Tem de ser um email!");
     
     else if(Idades.value === "")
         alert("Tem de preencher o campo idade!");//verifica se o campo não está vazio
     
-    else if(Idades.value<1 || Idades.value>100)//verifica se o a idade é valida
+    else if(Idades.value<4 || Idades.value>100)//verifica se o a idade é valida
         alert("Tem de inserir uma idade válida!");
 
     else if(Telefone.value==="")//verifica se o campo não está vazio
