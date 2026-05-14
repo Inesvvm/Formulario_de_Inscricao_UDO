959dac76873ae7e3f002dbc62a2fff02920c349d
Inesvvm <inesvvmoreira@gmail.com>
2026-05-14 16:35:08 +0100

    Verifica 1

--- app.js
+++ app.js
@@ -3,9 +3,39 @@ const Segunda_Pag=document.getElementById("Pag_2");//Busca o Elemeto com o Id Pa
 const Terceira_Pag=document.getElementById("Pag_3");//Busca o Elemeto com o Id Pag_3
 const Quarta_Pag=document.getElementById("Pag_4");//Busca o Elemeto com o Id Pag_4
 let i=1;
+let Pag1_certa=false;
 
+function Verifica1(){
+const Nome=document.getElementById("nome");
+const Data=document.getElementById("data_Nascimento");
+const Contribuinte=document.getElementById("NIF");
+
+    Nome.setCustomValidity(""); 
+        if(!Nome.checkValidity()){
+            Nome.style.border="2px solid red";
+            if(Nome.validity.valueMissing){
+                Nome.setCustomValidity("Tem de inserir um nome");
+                Nome.reportValidity();
+                return false;
+            }if(Nome.validity.tooShort){
+                Nome.setCustomValidity("Tem de ter no minimo 3 letras");
+                return false;
+            }
+        }if(Contribuinte.checkValidity()){
+            Contribuinte.style.border="2px solid red";
+            if(Contribuinte.valueMissing){
+                Contribuinte.checkValidity("Tem de Inserir o numero do contribuinte");
+                Nome.reportValidity();
+                return false;
+            }
+        }
+                return true;
+}
 //Inicio Fucção que vai para a página seguinte
     function Proxima_Pag(){
+        if(!Verifica1()){
+            return false;
+        }
         i++;
         if(i==1){
             Primeira_Pag.style.display='block';//Faz que a 1ª Pagina fique visivel
