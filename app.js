91f4d5b5620644dde00df28080789d60aeb64bb9
Inesvvm <inesvvmoreira@gmail.com>
2026-05-15 13:19:29 +0100

    Criação da função Tipos e TamanhosPatterns

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ let i=0;//Variavel para o conteudo da Seguinte_pagina
 
 function Letras(id){
     const So_Letras=document.getElementById(id);
+    if(So_Letras){
+        So_Letras.maxLength=150;
+        So_Letras.minLength=3;
+    }
+    
     So_Letras.oninput=function(){
         this.value=this.value.replace(/[^a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\s]/g, ''); //oniput serve para aceitar apaenas letras neste campo
     }
@@ -22,6 +27,64 @@ function Numeros(id){
         this.value=this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');
     }
 }//Nif, Cod_Postal, Tel, Tel_Pai, Tel_Mae, Tel_Urgente, Idade_Pai, Idade_Mae
+
+
+function Telefones(id){
+    const Telefone=document.getElementById(id);
+    if(Telefone){
+        Telefone.maxLength=9;
+        Telefone.minLength=9;
+    }
+}//Para Tel, Tel_Pai, Tel_Mae, Tel_Urgente
+
+
+function Idades(id){
+    const Idade=document.getElementById(id);
+    if(Idade){
+        Idade.maxLength=3;
+        Idade.minLength=1;
+    }
+}//Idade_Pai, Idade_Mae
+
+function Tipos(id, formato){
+    const Padrao= document.getElementById(id);
+    if(!Padrao){//Para se o campo não existir
+        return;
+    }
+
+    const Tel_Numeric={//serve para o formato bucar um destes valores
+        "Tel":{
+            inputMode="tel"
+            //Para Tel, Tel_Pai, Tel_Mae, Tel_Urgente
+        },
+        "Numeric":{
+            inputMode="numeric"
+            //Para NIF, codigo_Postal, Idade_Pai, Idade_Mae 
+        }
+    };
+}
+
+function Tamanhos_e_Patterns(id, tamanho_conteudo){
+    const tamanhoPattern=getElementById(id);
+    if(!tamanho){//Para se o campo não existir
+        return;
+    }
+    const Max_Min={
+        "Nove_digitos":{
+            maxLength=9,
+            minLength=8,
+            pattern: "[0-9]{9}"
+            //Para Nif,  Tel, Tel_Pai, Tel_Mae, Tel_Urgente
+        }
+    }
+}
+
+
+
+
+
+
+
 
 
 
