29eba183e8dcf331290c3a5a6e1e24c3375524e2
Inesvvm <inesvvmoreira@gmail.com>
2026-05-15 10:54:55 +0100

    Ajuste para tornar o código menos hardcoded (Mundança de página)
    Usado o querySelectorAll para selecionar a classe pagina que foi adicionada, o for each para aplicar em todos os elementos o display none, uso do for para reduzir código

--- app.js
+++ app.js
@@ -1,112 +1,30 @@
-const Primeira_Pag=document.getElementById("Pag_1");//Busca o Elemeto com o Id Pag_1
-const Segunda_Pag=document.getElementById("Pag_2");//Busca o Elemeto com o Id Pag_2
-const Terceira_Pag=document.getElementById("Pag_3");//Busca o Elemeto com o Id Pag_3
-const Quarta_Pag=document.getElementById("Pag_4");//Busca o Elemeto com o Id Pag_4
-let i=1;
-let Pag1_certa=false;
+const Seguinte_pagina=Array.from(document.querySelectorAll(".Paginas"));//O querySelectorAll seleciona todas as classes que tem Paginas
+Seguinte_pagina.forEach((pagina)=>{
+    pagina.style.display="none";
+});//Serve para aplicar em todos os elemetos da Pag_seguinte e pagina é uma varialvel temporaria com um nome a minha escolha
+Seguinte_pagina[0].style.display="block";//Serve para o Primeiro elemento  ficar visivel
 
-function Verifica1(){
-const Nome=document.getElementById("nome");
-const Data=document.getElementById("data_Nascimento");
-const Contribuinte=document.getElementById("NIF");
-const Cartao_Cidadao=document.getElementById("BI_CC");
+let cont=0;//Variavel para mudar de página
+let i=0;//Variavel para o conteudo da Seguinte_pagina
 
-    
-        if(!Nome.checkValidity()){
-            if(Nome.validity.valueMissing){
-                Nome.style.border="2px solid red";
-                Nome.setCustomValidity("Tem de inserir um nome");
-                Nome.reportValidity();
-                return false;
-            }if(Nome.validity.tooShort){
-                Nome.style.border="2px solid red";
-                Nome.setCustomValidity("Tem de ter no minimo 3 letras");
-                return false;
-            }
-
-        }if(!Data.checkValidity()){
-            if(Data.validity.valueMissing){
-                Data.style.border="2px solid red";
-                Data.setCustomValidity("Tem de Inserir uma data");
-                return false;
-            }if(Data.validity.patternMismatch){
-                Data.style.border="2px solid red";
-                Data.setCustomValidity("Tem de serguir opadrão recomendado");
-                return false;
-            }
-
-        }if(!Contribuinte.checkValidity()){
-            if(Contribuinte.validity.valueMissing){
-                Contribuinte.style.border="2px solid red";
-                Contribuinte.checkValidity("Tem de Inserir o numero do contribuinte");
-                Contribuinte.reportValidity();
-                return false;
-            }
-        }if(Cartao_Cidadao.checkValidity){
-            if(Cartao_Cidadao.valueMissing){
-                Cartao_Cidadao.style.border="2px solid red";
-                C
-            }
-        }
-        return true;
-}
 //Inicio Fucção que vai para a página seguinte
     function Proxima_Pag(){
-        if(!Verifica1()){
-            return false;
+        cont++;
+        for(i=0; i<Seguinte_pagina.length; i++){
+            Seguinte_pagina[i].style.display="none";
         }
-        i++;
-        if(i==1){
-            Primeira_Pag.style.display='block';//Faz que a 1ª Pagina fique visivel
-            Segunda_Pag.style.display='none';//Faz que a 2ª Pagina fique escondida
-            Terceira_Pag.style.display='none';//Faz que a 3ª Pagina fique escondida
-            Quarta_Pag.style.display='none';//Faz que a 4ª Pagina fique escondida
-
-        }else if(i==2){
-            Primeira_Pag.style.display='none';//Faz que a 1ª Pagina fique escondida
-            Segunda_Pag.style.display='block';//Faz que a 2ª Pagina fique visivel
-            Terceira_Pag.style.display='none';//Faz que a 3ª Pagina fique escondida
-            Quarta_Pag.style.display='none';//Faz que a 4ª Pagina fique escondida
-        }else if(i==3){
-            Primeira_Pag.style.display='none';//Faz que a 1ª Pagina fique escondida
-            Segunda_Pag.style.display='none';//Faz que a 2ª Pagina fique escondida
-            Terceira_Pag.style.display='block';//Faz que a 3ª Pagina fique visivel
-            Quarta_Pag.style.display='none';//Faz que a 4ª Pagina fique escondida
-        }else if(i==4){
-            Primeira_Pag.style.display='none';//Faz que a 1ª Pagina fique escondida
-            Segunda_Pag.style.display='none';//Faz que a 2ª Pagina fique escondida
-            Terceira_Pag.style.display='none';//Faz que a 3ª Pagina fique escondida
-            Quarta_Pag.style.display='block';//Faz que a 4ª Pagina fique visivel
-        }
-
+        Seguinte_pagina[cont].style.display="block";
     }
-//Inicio Fucção que vai para a página seguinte
+        
+//Fim Fucção que vai para a página seguinte
 
 //Inicio Fucção que vai para a página anterior
 function Pag_Anterior(){
-    i--;
-    if(i==1){
-        Primeira_Pag.style.display='block';//Faz que a 1ª Pagina fique visivel
-        Segunda_Pag.style.display='none';//Faz que a 2ª Pagina fique escondida
-        Terceira_Pag.style.display='none';//Faz que a 3ª Pagina fique escondida
-        Quarta_Pag.style.display='none';//Faz que a 4ª Pagina fique escondida
-
-    }else if(i==2){
-        Primeira_Pag.style.display='none';//Faz que a 1ª Pagina fique escondida
-        Segunda_Pag.style.display='block';;//Faz que a 2ª Pagina fique visivel
-        Terceira_Pag.style.display='none';//Faz que a 3ª Pagina fique escondida
-        Quarta_Pag.style.display='none';//Faz que a 4ª Pagina fique escondida
-    }else if(i==3){
-        Primeira_Pag.style.display='none';//Faz que a 1ª Pagina fique escondida
-        Segunda_Pag.style.display='none';//Faz que a 2ª Pagina fique escondida
-        Terceira_Pag.style.display='block';;//Faz que a 3ª Pagina fique visivel
-        Quarta_Pag.style.display='none';//Faz que a 4ª Pagina fique escondida
-    }else if(i==4){
-        Primeira_Pag.style.display='none';//Faz que a 1ª Pagina fique escondida
-        Segunda_Pag.style.display='none';//Faz que a 2ª Pagina fique escondida
-        Terceira_Pag.style.display='none';//Faz que a 3ª Pagina fique escondida
-        Quarta_Pag.style.display='block';;//Faz que a 4ª Pagina fique visivel
+    cont--;
+    for(i=0; i<Seguinte_pagina.length; i++){
+        Seguinte_pagina[i].style.display="none";
     }
+    Seguinte_pagina[cont].style.display="block";
 }
 //Fim Funcção que vai para a página seguinte
 
